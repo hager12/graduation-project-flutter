f69fde0413656048d26f399e3ce9acbfa855d036
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-04 11:00:46 +0200

    Update admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,14 +1,14 @@
 const adminRouter = require('express').Router();
 const adminController = require('../controllers/admin.controller');
-const authToken = require('../middleware/adminAuth');
+const adminAuth = require('../middleware/adminAuth');
 
 
-adminRouter.get('/admin', authToken, adminController.home);
-// adminRouter.post('/adminLogin', authToken, adminController.adminLogin);
-adminRouter.get('/allEmployee', authToken, adminController.allEmployee);
-adminRouter.get('/allHospital', authToken, adminController.allHospital);
-adminRouter.post('/addEmployee', authToken, adminController.addEmployee);
-adminRouter.put('/updateEmployee', authToken, adminController.updateEmployee);
-adminRouter.delete('/deleteEmployee', authToken, adminController.deleteEmployee);
+adminRouter.get('/admin', adminAuth, adminController.home);
+// adminRouter.post('/adminLogin', adminAuth, adminController.adminLogin);
+adminRouter.get('/allEmployee', adminAuth, adminController.allEmployee);
+adminRouter.get('/allHospital', adminAuth, adminController.allHospital);
+adminRouter.post('/addEmployee', adminAuth, adminController.addEmployee);
+adminRouter.put('/updateEmployee', adminAuth, adminController.updateEmployee);
+adminRouter.delete('/deleteEmployee', adminAuth, adminController.deleteEmployee);
 
 module.exports = adminRouter;
