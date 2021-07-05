9bfb08e8ce80e9d6143fa03b9ad0d1b70ea2bdd7
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-05 19:43:44 +0200

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -16,6 +16,6 @@ userRouter.post('/addReview', userController.addReview);
 userRouter.put('/updateReview', userAuth, userController.updateReview);
 userRouter.delete('/deleteReview', userAuth, userController.deleteReview);
 userRouter.post('/departmentsByHosID', userAuth, userController.departmentsByHosID);
-userRouter.post('/booking', userAuth, userController.booking);
+userRouter.put('/booking', userAuth, userController.booking);
 
 module.exports = userRouter;
