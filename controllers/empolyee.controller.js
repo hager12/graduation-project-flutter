eb3fcce4a0c7743c8a10f1edddaeba75bae4cbb7
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-06 13:39:44 +0200

    Update empolyee.controller.js

--- controllers/empolyee.controller.js
+++ controllers/empolyee.controller.js
@@ -43,7 +43,7 @@ module.exports.updateHospital = async (req, res)=>{
     }
 };
 module.exports.deleteHospital = async (req, res)=>{
-    let _id = req.body._id;
+    let _id = req.header('_id');
     try {
         await hospitalModel.findOneAndDelete({_id})
         res.json("Deleted");
