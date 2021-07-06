3f01d05c467c5d0c1efe18d9f5a6c84f4b219d3a
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-06 12:48:59 +0200

    Update employee.route.js

--- routes/employee.route.js
+++ routes/employee.route.js
@@ -3,7 +3,7 @@ const employeeController = require('../controllers/empolyee.controller')
 const empAuth = require('../middleware/empAuth');
 
 employeeRouter.get('/employee', empAuth, employeeController.home);
-employeeRouter.get('/allHospitals', empAuth, employeeController.allHospitals);
+employeeRouter.get('/allHospitalEmp', empAuth, employeeController.allHospitals);
 employeeRouter.get('/allReviews', empAuth, employeeController.allReviews);
 employeeRouter.post('/addHospital', empAuth, employeeController.addHospital);
 employeeRouter.put('/updateHospital', empAuth, employeeController.updateHospital)
