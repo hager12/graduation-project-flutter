5fb78f699ba9b9a8578cfcfbaca7f266a662a1e1
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-06 17:40:47 +0200

    Update admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -7,6 +7,7 @@ adminRouter.get('/admin', adminController.home);
 // adminRouter.post('/adminLogin', adminAuth, adminController.adminLogin);
 adminRouter.get('/allEmployee', adminAuth, adminController.allEmployee);
 adminRouter.get('/allHospital', adminAuth, adminController.allHospital);
+adminRouter.post('/getEmpByID', adminAuth, adminController.getEmpByID);
 adminRouter.post('/addEmployee', adminAuth, adminController.addEmployee);
 adminRouter.put('/updateEmployee', adminAuth, adminController.updateEmployee);
 adminRouter.delete('/deleteEmployee', adminAuth, adminController.deleteEmployee);
