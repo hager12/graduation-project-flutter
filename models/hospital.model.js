bc2f1417d8a54a1e2c9e3020c9c52d70efbdd3e0
Bica19 <45574935+Bica19@users.noreply.github.com>
2021-07-06 14:50:47 +0200

    Update hospital.model.js

--- models/hospital.model.js
+++ models/hospital.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 var hospitalSchema =  new mongoose.Schema({
-    name: {type:String, required:true},
+    name: {type:String, require:true},
     location: {type:String, require:true},
-    description: {type:String, required:true},
+    description: {type:String, require:true},
     phoneNumber: {type:String, require:true},
-    rate: {type:String, required:true},
+    rate: {type:String, require:true},
     picture: {type:String, require:true},
     leet: {type:String, require:true},
-    lang: {type:String, required:true},
+    lang: {type:String, require:true},
     webURL: {type:String, require:true}
 });
 module.exports = mongoose.model('Hospital', hospitalSchema);
